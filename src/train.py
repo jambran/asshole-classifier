c33a88b541b170cc1e074d2e9409c6ab7b8c8252
jambran <jbrandon@brandeis.edu>
2021-02-16 19:47:34 -0500

    experiment run

--- src/train.py
+++ src/train.py
@@ -11,24 +11,29 @@ from model import AssholeClassifier
 from reddit_data_module import RedditDataModule
 
 if __name__ == '__main__':
-    for lr in [.03, .01, .003, .001]:
-        exp_name = f'title-lr={lr}'
-        print(f"starting exp {exp_name}")
-        torch.manual_seed(2)
-        train_dir = os.path.join('..', 'data', 'raw')
-        data_module = RedditDataModule(data_dir=train_dir,
-                                       data_filename='10000.csv')
-        model = AssholeClassifier(learning_rate=lr,
-                                  possible_labels=[0, 1])
-        trainer = pl.Trainer(
-            default_root_dir='logs',
-            gpus=0,  # (1 if torch.cuda.is_available() else 0),
-            max_epochs=10,
-            fast_dev_run=exp_name == 'debug',
-            logger=pl.loggers.TensorBoardLogger('logs', name=exp_name, version=0),
-            callbacks=[ModelCheckpoint(save_top_k=-1,
-                                       save_last=True),
-                       ]
-        )
-        trainer.fit(model, data_module)
-        print('all done')
+    # for lr in [.03, .01, .003, .001]:
+
+    # could try text
+    # could try title + tldr
+    # could try title + text
+    lr = .01
+    exp_name = f'text-lr={lr}'
+    print(f"starting exp {exp_name}")
+    torch.manual_seed(2)
+    train_dir = os.path.join('..', 'data', 'raw')
+    data_module = RedditDataModule(data_dir=train_dir,
+                                   data_filename='10000.csv')
+    model = AssholeClassifier(learning_rate=lr,
+                              possible_labels=[0, 1])
+    trainer = pl.Trainer(
+        default_root_dir='logs',
+        gpus=0,  # (1 if torch.cuda.is_available() else 0),
+        max_epochs=10,
+        fast_dev_run=exp_name == 'debug',
+        logger=pl.loggers.TensorBoardLogger('logs', name=exp_name, version=0),
+        callbacks=[ModelCheckpoint(save_top_k=-1,
+                                   save_last=True),
+                   ]
+    )
+    trainer.fit(model, data_module)
+    print('all done')
